271ea35d439e41f9367861a75ea1bc463d77509a
Allen Shibu <allenshibukuriakose@gmail.com>
2026-02-13 18:11:27 +0530

    header issue in width sizze less than 400 and no of products in md fixed

--- marketplace.js
+++ marketplace.js
@@ -22,7 +22,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const MainContent = document.getElementById("main");
   const BackDrop = document.getElementById("sidebar-backdrop")
   const Header=document.getElementById("header")
+  const MobileWidth=document.getElementById("mobilelesswidth")
 
+  if (window.innerWidth < 400) {
+    MobileWidth.classList.replace("gap-5","gap-3")
+  }
 
   ProfileBtn.addEventListener("click", async (e) => {
     const { data: { user }, error } = await supabase.auth.getUser();
@@ -260,7 +264,7 @@ async function loadProducts(SearchResults=null) {
 
       card.innerHTML = `
       <div class="flex flex-row relative overflow-hidden rounded-2xl">
-          <img src="${product.image_url}" class="w-full h-64 object-cover hover:scale-105 transition-transform duration-300">
+          <img src="${product.image_url}" class="w-full md:h-64 h-50 object-cover hover:scale-105 transition-transform duration-300">
           <button class="wishlist-btn absolute top-3 right-3 bg-white/90 backdrop-blur-sm p-2 rounded-full ${isLiked ? "text-red-500" : "text-gray-400"} hover:text-red-500 transition-colors shadow-sm" data-id="${product.id}">
             <svg xmlns="http://www.w3.org/2000/svg" ${isLiked ? 'fill="currentColor"' : 'fill="none"'} viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 pointer-events-none">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />

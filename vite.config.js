c453c64c1f703082d3c16b4572306f960c83de99
Allen Shibu <allenshibukuriakose@gmail.com>
2026-02-16 20:27:35 +0530

    fixing hosting issues

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,15 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        sidebar_header: resolve(__dirname, "main.html"),
         main: resolve(__dirname, "index.html"),
         login: resolve(__dirname, "login.html"),
         market: resolve(__dirname, "market-place.html"),
         selling: resolve(__dirname, "sellingpage.html"),
         listing: resolve(__dirname, "listing.html"),
         wishlist: resolve(__dirname, "wishlist.html"),
-        contact: resolve(__dirname, "contact.html"),        forum: resolve(__dirname, "forum.html"),
+        contact: resolve(__dirname, "contact.html"),
+        forum: resolve(__dirname, "forum.html"),
       },
     },
   },

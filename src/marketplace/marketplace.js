71a075310fdd016ffd327a639e517ab32e353fbe
Allen Shibu <allenshibukuriakose@gmail.com>
2026-02-24 16:46:37 +0530

    changes

--- src/marketplace/marketplace.js
+++ src/marketplace/marketplace.js
@@ -89,7 +89,7 @@ async function loadProducts(SearchResults = null) {
       card.innerHTML = `
       <div class="border border-gray-400 p-5 rounded-2xl dark:bg-[#191b1f] bg-gray-100/70 dark:border-0 transition-all duration-300" id="productbox">
       <div class="flex flex-row relative overflow-hidden rounded-2xl">
-          <img src="${product.image_url}" class="w-full md:h-64 h-50 object-cover hover:scale-102 transition-transform duration-300">
+          <img src="${product.image_url[0] || product.image_url}" class="w-full md:h-64 h-50 object-cover hover:scale-102 transition-transform duration-300">
           <button class="wishlist-btn cursor-pointer absolute top-3 right-3 bg-white/30 backdrop-blur-sm p-2 rounded-full ${isLiked ? "text-red-500" : "text-gray-400"} hover:text-red-500 transition-colors shadow-sm" data-id="${product.id}">
             <svg xmlns="http://www.w3.org/2000/svg" ${isLiked ? 'fill="currentColor"' : 'fill="none"'} viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 pointer-events-none">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />

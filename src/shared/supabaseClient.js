13759fefbc4ab8b9a54bbfd04be47b77d05e27db
Allen Shibu <allenshibukuriakose@gmail.com>
2026-02-28 09:16:36 +0530

    fiixed supabase IPS issue.Outh doesnt work

--- src/shared/supabaseClient.js
+++ src/shared/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://opzechnwukuqfvfaytfx.supabase.co";
+const supabaseUrl = 'https://univo.jiobase.com';
 const supabaseKey = "sb_publishable_w87ezT-fcldEAO653BOpwQ_UvJtX51_";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// "https://opzechnwukuqfvfaytfx.supabase.co"
